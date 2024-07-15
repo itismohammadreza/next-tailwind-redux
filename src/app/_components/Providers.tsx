176abcc666f51aff8fff34e579e8f029085927df
mohammadreza99 <shahbaziweb@gmail.com>
2024-07-15 13:05:54 +0330

    install primereact

--- src/app/_components/Providers.tsx
+++ src/app/_components/Providers.tsx
@@ -3,15 +3,18 @@ import { Loading } from "./Loading";
 import { Toast } from "./Toast";
 import { StoreProvider } from "@redux/StoreProvider";
 import { PropsWithChildren } from "react";
+import { PrimeReactProvider } from "primereact/api";
 import '@locales/i18n';
 import "@styles/global.scss";
 
 export const Providers = ({children}: PropsWithChildren) => {
   return (
       <StoreProvider>
-        <Loading/>
-        <Toast/>
-        {children}
+        <PrimeReactProvider value={{ripple: true}}>
+          <Loading/>
+          <Toast/>
+          {children}
+        </PrimeReactProvider>
       </StoreProvider>
   );
 }
